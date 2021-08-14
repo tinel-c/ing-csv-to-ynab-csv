36d4e148b80ab700c10c0fbcfb7a106573bc8fc0
Constantin Bogza <tinel.c@gmail.com>
2021-08-14 12:33:58 +0300

    update to python 3 and update to the new budget
    Add additional providers

--- ing_convert_to_ynab.py
+++ ing_convert_to_ynab.py
@@ -6,16 +6,29 @@ import glob
 mappingCategory = [('CARREFOUR', 'Everyday Expenses: Groceries'),                    # Carrefour
                    ('MPN LOGISTICS', 'Everyday Expenses: Groceries'),                # Carrefour
                    ('Zig Delivero', 'Everyday Expenses: Groceries'),                 # Carrefour
+                   ('ROUMASPORT', 'Everyday Expenses: Groceries'),                   # Carrefour Bringo
+                   ('RAILEANU TRANSPORT', 'Everyday Expenses: Groceries'),           # Carrefour Bringo
+                   ('CRISNIC MARKET', 'Everyday Expenses: Groceries'),               # Pico
                    ('LIDL', 'Everyday Expenses: Groceries'),                         # LIDL
                    ('PICO MAG DEP  RO', 'Everyday Expenses: Groceries'),             # PiCo magazin
                    ('MARELBO', 'Everyday Expenses: Haine'),                          # Marelbo
-                   ('MY EVENTS SRL RO', 'Everyday Expenses: Office food'),           # Bistro Servici
+                   ('MY EVENTS SRL', 'Everyday Expenses: Office food'),              # Bistro Servici
                    ('SALAD BOX', 'Everyday Expenses: Office food'),                  # Bistro Servici
                    ('MADO', 'Everyday Expenses: Office food'),                       # Bistro Servici
                    ('OSPATUL ZEILOR', 'Everyday Expenses: Office food'),             # Bistro Servici
                    ('SIMIGERIA PETRU', 'Everyday Expenses: Office food'),            # Bistro Servici
+                   ('Dinah  Aliment', 'Everyday Expenses: Restaurants'),             # Restaurante
+                   ('VO CHEF', 'Everyday Expenses: Restaurants'),                    # Restaurante
+                   ('CARTOFISSERIE', 'Everyday Expenses: Restaurants'),              # Restaurante
+                   ('CANTINA STUDENTEASCA', 'Everyday Expenses: Restaurants'),       # Restaurante
+                   ('MESOPOTAMIA', 'Everyday Expenses: Restaurants'),                # Restaurante
+                   ('CORSO', 'Everyday Expenses: Restaurants'),                # Restaurante
+                   ('VARUNA', 'Everyday Expenses: Toys'),                            # Magazin Jucarii
                    ('EON GAZ FURNZARE', 'Monthly Bills: EON'),                       # EON Natural Gaz
                    ('WWW.MYLINE-EON.RO', 'Monthly Bills: EON'),                      # Eon myline payment
+                   ('HBO', 'Monthly Bills: HBOgo'),                                  # HBO GO
+                   ('ORANGE', 'Monthly Bills: Phone'),                               # Orange Romania
+                   ('Scoala Primara EuroEd','Monthly Bills: Kindergarden&School'),   # KinderGarden and School - Euroed
                    ('EMAG SHOWR', 'Everyday Expenses: Household Goods'),             # EMAG
                    ('DM FIL', 'Everyday Expenses: Household Goods'),                 # DM
                    ('ALTEX', 'Everyday Expenses: Household Goods'),                  # Altex
@@ -29,6 +42,7 @@ mappingCategory = [('CARREFOUR', 'Everyday Expenses: Groceries'),               
                    ('BANGGOOD', 'Monthly Bills: Atelier'),                           # Bangood
                    ('ARCADIA', 'Everyday Expenses: Medical'),                        # Arcadia Hospital
                    ('ARTIMA', 'Everyday Expenses: Medical'),                         # Farmacie ARTIMA
+                   ('ROPHARMA', 'Everyday Expenses: Medical'),                       # Farmacie ROPHARMA
                    ('HELP NET FARMA', 'Everyday Expenses: Medical'),                 # Farmacie Helpnet
                    ('SMILE DENT SRL', 'Everyday Expenses: Medical'),                 # Dentist
                    ('FITERMAN', 'Everyday Expenses: Medical'),                       # Farmacie Fitterman
@@ -39,7 +53,6 @@ mappingCategory = [('CARREFOUR', 'Everyday Expenses: Groceries'),               
                    ('H&M', 'Everyday Expenses: Haine'),                              # H&M
                    ('INA IULIUS MALL', 'Everyday Expenses: Haine'),                  # INA Iulius Mall 
                    ('SIA SOF', 'Everyday Expenses: Haine'),                          # SIA SOF
-                   ('HBO', 'Monthly Bills: HBOgo'),                                  # HBO GO
                    ('PASTICCERIA MONTECAT', 'Everyday Expenses: Restaurante'),       # Montecatini
                    ('PREMIER RESTAURANTS', 'Everyday Expenses: Restaurante'),        # McDonalds
                    ('MOO CAFE PALAS DEP', 'Everyday Expenses: Restaurante'),         # Moo
@@ -48,19 +61,20 @@ mappingCategory = [('CARREFOUR', 'Everyday Expenses: Groceries'),               
                    ('KFC', 'Everyday Expenses: Restaurante'),                        # KFC
                    ('LAVA&CUCE', 'Everyday Expenses: Curatenie & Ironing'),          # Curatatorie Iulius
                    ('BLISS BEAUTY', 'Everyday Expenses: Curatenie & Ironing'),       # Bliss
-                   ('Rata Credit In contul: 9999', 'Debt: Credit apartament Buni'),  # ING Credit
-                   ('Prima asigurare ING Credit', 'Debt: Credit apartament Buni'),   # Asigurare ING Credit
-                   ('ORANGE ROMANIA', 'Monthly Bills: Telefon Tinel'),               # Orange Romania
+                   ('Rata Credit In contul: 9999', 'Debt: Apartment payment'),       # ING Credit
+                   ('Prima asigurare ING Credit', 'Debt: Apartment payment'),        # Asigurare ING Credit
                    ('LEROY MERLIN ROMANIA SRL', 'Rainy Day Funds: Home Maintenance'),# Home Maintenance
                    ('DEDEMAN', 'Rainy Day Funds: Home Maintenance'),                 # Home Maintenance
                    ('Support Auto', 'Rainy Day Funds: Car Repairs'),                 # Home Maintenance
-                   ('GLOBEHOSTIN', 'Yearly bills: Site-uri'),                        # edomenii.ro
-                   ('MUNICIPIUL IASI SNEP  RO', 'Yearly bills: Impozite'),           # Impozit apartament
-                   ('ETAX', 'Yearly bills: Impozite'),                               # Impozit apartament ETAX
+                   ('GLOBEHOSTIN', 'Savings Goals: Domains'),                        # edomenii.ro
+                   ('MUNICIPIUL IASI SNEP  RO', 'Yearly bills: Taxes'),              # Impozit apartament
+                   ('ETAX', 'Yearly bills: Taxes'),                                  # Impozit apartament ETAX
+                   ('STEAM GAMES', 'Everyday Expenses: Spending Money Tinel'),       # Joace
                    ('Retragere numerar', 'Everyday Expenses: Spending Money'),       # Extrageri
-                   ('Transfer Home','Rainy Day Funds: Emergency Fund'),              # Saving
+                   ('Tranzactie Round Up','Debt: Emergency Fund'),                   # Saving
                    ('Realimentare Extra', 'ignore'),                                 # ignore transaction
-                   ('transferata din linia de credit', 'ignore')]                    # ignore transaction
+                   ('transferata din linia de credit', 'ignore'),                    # ignore transaction
+                   ('Detalii tranzactie', 'ignore')]                                 # ignore transaction
 
 detalii_tranzactie = ""
 debit = ""
@@ -83,7 +97,7 @@ for filename in glob.glob('*.csv'):
     witeToFilename = "ynab_" + filename
     # create a file by appending ynab_ in front of the opened filename
     data = ""
-    with open(witeToFilename, 'wb') as csvfile:
+    with open(witeToFilename, 'w') as csvfile:
         fieldnames = ['Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -95,7 +109,7 @@ for filename in glob.glob('*.csv'):
                     # reached the end of a segment. start of a segment is marked by populated date.
                     # end of a segmend considered when another segment begins
                     if data:
-                        print 'output: ' + data + ',' + detalii_tranzactie + ',"' + debit + '","' + credit + '"'
+                        print ('output: ' + data + ',' + detalii_tranzactie + ',"' + debit + '","' + credit + '"')
                         # try to identify category
                         category = findCategory(detalii_tranzactie)
                         # if category is ignore do not write something
@@ -109,7 +123,7 @@ for filename in glob.glob('*.csv'):
                     printData = '----->' + row['Data'] + ','
                     printData += row['Detalii tranzactie'] + ',"'
                     printData += row['Debit'] + '","' + row['Credit'] + '"'
-                    print printData
+                    print (printData)
                     data = row['Data']
                     # Replace the romanian month by the number of the month
                     data = data.replace(" ianuarie ", "/01/")
@@ -131,7 +145,7 @@ for filename in glob.glob('*.csv'):
                     detalii_tranzactie = detalii_tranzactie + \
                         ' ' + row['Detalii tranzactie']
             # catch also the last entry inside the import file
-            print 'output: ' + data + ',' + detalii_tranzactie + ',"' + debit + '","' + credit + '"'
+            print ('output: ' + data + ',' + detalii_tranzactie + ',"' + debit + '","' + credit + '"')
             # try to identify category
             category = findCategory(detalii_tranzactie)
             # write the entry in YNAB csv
